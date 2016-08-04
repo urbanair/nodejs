42b8e50a7ff8e576bc889c6900acf01f9e2782e4
urbanair <dennis@urbanairdesign.com>
2016-08-04 10:45:13 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var app = require('express').createServer();
-app.get('/', function(req, res) {
-  res.send("Hello World");
-});
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("listening on 3000");
-});
