e2e4cc68a2ce7e3089894260269698e1394c68e7
urbanair <dennis@urbanairdesign.com>
2016-08-04 09:51:50 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
+  response.send('Hello World!');
 });
 
 app.listen(app.get('port'), function() {
