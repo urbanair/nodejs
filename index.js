a96956d6c044f79f64da15a1fcfc30d04f9fd330
urbanair <dennis@urbanairdesign.com>
2016-08-04 11:07:38 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000;
 
-app.set('port', (process.env.PORT || 5000));
-
+/*
 app.get('/', function(request, response) {
   response.send("Hello World");
 });
@@ -10,3 +10,18 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+*/
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.send('home page!');
+});
+
+router.get('/about', function(req, res) {
+	res.send('about page!');
+});
+
+app.use('/', router);
+
+app.listen(port);
